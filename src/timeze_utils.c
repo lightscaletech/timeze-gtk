043788e5cc7635e93bc863109c4ac619357aec93
Sam Light <sam@lightscale.co.uk>
2019-04-02 11:18:42 +0100

    Create utils file with function to free string lists.

---
+++ src/timeze_utils.c
@@ -0,0 +1,6 @@
+#include "timeze_utils.h"
+
+void
+timeze_string_list_free(gpointer str) {
+    g_free(str);
+}
---
+++ src/timeze_utils.h
@@ -0,0 +1,9 @@
+#ifndef _TIMEZE_UTILS_H_
+#define _TIMEZE_UTILS_H_
+
+#include <glib.h>
+
+void
+timeze_string_list_free(gpointer);
+
+#endif
